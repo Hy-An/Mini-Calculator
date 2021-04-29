ced7cde1ccee8c280aef0571c72c04b4b2f0d05e
Exequielt <exequield.torres@gmail.com>
2021-04-29 15:11:31 -0300

    mini calculadora

--- Script.js
+++ Script.js
@@ -1,1 +1,6 @@
-document.write("<h2>puto el que lee<h2>");
+var num1 = window.prompt("Ingrese numero");
+var num2 = window.prompt("Ingrese otro numero");
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+document.write("El resultado de la suma es: ",num1 + num2);
