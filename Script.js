3fd20751f8fbd85bdfae4f861eacde1347591aed
Exequielt <exequield.torres@gmail.com>
2021-04-29 16:59:32 -0300

    optimizado

--- Script.js
+++ Script.js
@@ -1,6 +1,4 @@
-var num1 = window.prompt("Ingrese numero");
-var num2 = window.prompt("Ingrese otro numero");
+const num1 = parseInt(window.prompt("Ingrese numero"));
+const num2 = parseInt(window.prompt("Ingrese otro numero"));
 
-num1 = parseInt(num1);
-num2 = parseInt(num2);
 document.write("El resultado de la suma es: ",num1 + num2);
